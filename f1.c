e5c860cea75b3eace40613666c629a94e37228f4
Parth Soni <76111886+parth-soni-10@users.noreply.github.com>
2022-05-24 19:31:41 +0530

    Add files via upload

---
+++ f1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the values of a and b:- ");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("Multiplication is :- %d",c);
+	return 0;
+}
+
---
+++ f2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	writef("Enter the values of a and b:- ");
+	readf("%d%d",&a,&b);
+	c=a*b;
+	writef("Multiplication is :- %d",c);
+	return 0;
+}
+
